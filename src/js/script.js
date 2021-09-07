e4c5e33aae2f4895720a15dab24c948047847d1a
Dyx1s <grafdonskiy@gmail.com>
2021-09-07 14:02:01 +0300

    update code and files, add arrowup in script.js

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+"use sctrict";
+
+
+/* const result = confirm ("are you sure?");
+console.log(result); */
+
+/* const answer = +prompt('Вам есть 18?', '18');
+console.log(typeof (answer)); */
+
+const answers = [];
+/* answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше Фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', ''); */
+
+console.log(typeof(answers));
